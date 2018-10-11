576713034a01bd6e94f059de00529fe0e7957ee9
LPcarpe <a2895222821@163.com>
2018-10-11 08:55:58 +0800

    新文件

--- flash动画总结/完美运动框架.js
+++ flash动画总结/完美运动框架.js
@@ -1,47 +1,45 @@
-function getStyle (obj, attr){
-	if(obj.currentStyle){
+function getStyle(obj, attr) {
+	if (obj.currentStyle) {
 		return obj.currentStyle[attr];
-	}
-	else{
-		return getComputedStyle(obj,false)[attr];
+	} else {
+		return getComputedStyle(obj, false)[attr];
 	}
 }
-function startMove(obj, json, fn){
+
+function startMove(obj, json, fn) {
 	clearInterval(obj.timer);
 
-	obj.timer = setInterval(function (){
+	obj.timer = setInterval(function () {
 
 		var flag = true;
-		for(var attr in json){
+		for (var attr in json) {
 			var iCur = 0;
-			if (attr == 'opacity'){
-				iCur = Math.round(parseFloat(getStyle(obj,attr)*100))
-			}
-			else{
+			if (attr == 'opacity') {
+				iCur = Math.round(parseFloat(getStyle(obj, attr) * 100))
+			} else {
 				iCur = parseInt(getStyle(obj, attr));
 			}
 
-			var iSpeed = (json[attr] - iCur)/8;
-			var speed = iSpeed>0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
+			var iSpeed = (json[attr] - iCur) / 8;
+			var speed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
 
-			if(iCur != json[attr]){
+			if (iCur != json[attr]) {
 				flag = false;
 			}
-			if(attr == 'opacity'){
-				obj.style.filter = 'alpha(opacity:'+(iCur+speed)+')';
-				obj.style.opacity = (iCur+speed)/100;
-			}
-			else{
-				obj.style[attr] = iCur+speed+'px';
+			if (attr == 'opacity') {
+				obj.style.filter = 'alpha(opacity:' + (iCur + speed) + ')';
+				obj.style.opacity = (iCur + speed) / 100;
+			} else {
+				obj.style[attr] = iCur + speed + 'px';
 			}
 
-			if(flag){
+			if (flag) {
 				clearInterval(obj.timer);
-				if(fn){
+				if (fn) {
 					fn();
 				}
 			}
 		}
 
-	},30)
+	}, 30)
 }
